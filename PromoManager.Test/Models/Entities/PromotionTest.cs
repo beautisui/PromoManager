ed45c10d112549ecd8550c97092a24a959f26b1a
Beauti Sui <183709979+beautisui@users.noreply.github.com>
2025-06-17 17:15:50 +0530

    Creates a Test project

--- PromoManager.Test/Models/Entities/PromotionTest.cs
+++ PromoManager.Test/Models/Entities/PromotionTest.cs
@@ -1,7 +1,15 @@
+using PromoManager.Models.Entities;
+
 namespace PromoManager.Test.Models.Entities;
 
 [TestFixture]
+[TestOf(typeof(Promotion))]
 public class PromotionTest
 {
-    
+
+    [Test]
+    public void METHOD()
+    {
+        
+    }
 }
