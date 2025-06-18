dfcef14c024d4c163ff16b25551d0f8bb2393565
Beauti Sui <183709979+beautisui@users.noreply.github.com>
2025-06-18 14:40:06 +0530

    Implements Repository logic for fetching data from Db

--- PromoManager/Repository/PromoRepository.cs
+++ PromoManager/Repository/PromoRepository.cs
@@ -1,16 +1,48 @@
 using PromoManager.Models.Entities;
+using System.Data;
+using Dapper;
+using Microsoft.Data.Sqlite;
 
 namespace PromoManager.Repository;
 
 public class PromoRepository : IPromoRepository
 {
-    public Task<IEnumerable<Promotion>> GetAllPromotions()
+    private readonly IConfiguration _configure;
+
+    public PromoRepository(IConfiguration configuration)
     {
-        throw new NotImplementedException();
+        _configure = configuration;
     }
 
-    public Task<Promotion> AddPromotion(Promotion promotion)
+    private IDbConnection CreateConnection()
     {
-        throw new NotImplementedException();
+        Console.WriteLine($"DB file path: {_configure.GetConnectionString("DefaultConnection")}");
+        return new SqliteConnection(_configure.GetConnectionString("DefaultConnection"));
+    }
+    
+    public async Task<IEnumerable<Promotion>> GetAllPromotions()
+    {
+        using var connection = CreateConnection();
+        var query = "SELECT * FROM Promotions;";
+        return await connection.QueryAsync<Promotion>(query);
+    }
+
+
+    public async Task<Promotion> AddPromotion(Promotion promotion)
+    {
+        using var connection = CreateConnection();
+
+        var query = @"
+            INSERT INTO Promotions (Item, Store, StartDate, EndDate, Tactic) 
+            VALUES (@Item, @Store, @StartDate, @EndDate, @Tactic);
+            SELECT last_insert_rowid()";
+
+        var insertedId = await connection.ExecuteScalarAsync<long>(query, promotion);
+
+        var recentlyAddedPromo = await connection.QuerySingleAsync<Promotion>(
+            "SELECT * FROM Promotions WHERE PromoId = @Id",
+            new { Id = insertedId });
+
+        return recentlyAddedPromo;
     }
 }
