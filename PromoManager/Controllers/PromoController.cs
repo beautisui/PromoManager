0e2996019c25b3961552dc314aac1ac7eac1dbbd
Beauti Sui <183709979+beautisui@users.noreply.github.com>
2025-06-18 14:38:17 +0530

    Implements Controller logic for Api request

--- PromoManager/Controllers/PromoController.cs
+++ PromoManager/Controllers/PromoController.cs
@@ -1,6 +1,32 @@
-namespace PromoManager.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using PromoManager.Models.Entities;
+using PromoManager.Service;
 
-public class PromoController
+namespace PromoManager.Controllers
 {
-    
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PromotionController : ControllerBase
+    {
+        private readonly IPromoService _promoService;
+
+        public PromotionController(IPromoService promoService)
+        {
+            _promoService = promoService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Promotion>>> GetPromotions()
+        {
+            var result = await _promoService.GetPromotions();
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Promotion>> AddPromotion([FromBody] Promotion promotion)
+        {
+            var result = await _promoService.AddPromotion(promotion);
+            return CreatedAtAction(nameof(GetPromotions), new { id = result.PromoId }, result);
+        }
+    }
 }
