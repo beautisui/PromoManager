19965befe1b31d2e6fd03cbf253d0163dd499267
Beauti Sui <183709979+beautisui@users.noreply.github.com>
2025-06-18 14:34:39 +0530

    Add table creation query to Program.cs if table does not exist

--- PromoManager/Program.cs
+++ PromoManager/Program.cs
@@ -1,27 +1,58 @@
+using Dapper;
+using Microsoft.Data.Sqlite;
+using PromoManager.Repository;
+using PromoManager.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IPromoRepository, PromoRepository>();
+builder.Services.AddScoped<IPromoService, PromoService>();
+
+try
+{
+    using (var connection = new SqliteConnection(builder.Configuration.GetConnectionString("DefaultConnection")))
+    {
+        connection.Open();
+        Console.WriteLine("Connection opened to DB file.");
+        Console.WriteLine($"DB file path: {builder.Configuration.GetConnectionString("DefaultConnection")}");
+        
+        var tableCommand = @"
+        CREATE TABLE IF NOT EXISTS Promotions (
+            PromoId INTEGER PRIMARY KEY AUTOINCREMENT,
+            Item TEXT NOT NULL,
+            Store TEXT NOT NULL,
+            StartDate TEXT NOT NULL,
+            EndDate TEXT NOT NULL,
+            Tactic TEXT NOT NULL
+        );";
+
+        connection.Execute(tableCommand);
+        connection.Execute("select * from Promotions;");
+        Console.WriteLine("Executed CREATE TABLE command.");
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Error while creating DB/table: " + ex.Message);
+}
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
